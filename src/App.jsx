b2f2788f6fd89316cfb1dcc8431dde6ca0e1454f
rawdydev <dadullahakbary@gmail.com>
2025-02-09 23:46:29 +0330

    setuped react and tailwind in the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <h1 class="text-3xl font-bold underline text-blue-800">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
